8edd5272593b8f8f30a118439aee434905faa68e
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-12-31 00:37:19 -0800

    UserController

---
+++ Backend/models/UserModels.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jsonwebtoken = require('jsonwebtoken')
+
+const UserSchema = new mongoose.Schema({
+
+    fullname: {
+        firstname: {
+            type: String,
+            required: true,
+            minLength: [3, "First Name has to be atleast 3 characters long"]
+        },
+        lastname: {
+            type: String,
+            minLength: [3, " Last Name has to be atleast 3 characters long"]
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        minLength: [5, " Email has to be atleast 5 characters long"]
+
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    socketId: {
+        type: String
+    }
+
+})
+
+UserSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this_id }, process.env.JWT_SECRET)
+    return token
+}
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+UserSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+};
+
+const userModel = mongose.model('User', UserSchema)
+
+module.exports = userModel 
